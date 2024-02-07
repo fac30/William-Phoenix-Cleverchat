be3b1f0dc5faa0542262cbe516ab92695f0af693
RayFFH <57190209+RayFFH@users.noreply.github.com>
2024-02-07 21:10:31 +0000

    Installed nodeman package

--- deploy-commands.js
+++ deploy-commands.js
@@ -32,7 +32,7 @@ for (const folder of commandFolders) {
 }
 
 // Construct and prepare an instance of the REST module
-const rest = new REST().setToken(process.env.PERSONAL_TOKEN);
+const rest = new REST().setToken(process.env.DISCORD_TOKEN);
 
 // and deploy your commands!
 (async () => {
